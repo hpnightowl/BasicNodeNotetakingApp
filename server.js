d1a7dae1b88910bef9c8c91b41baf6bad04f57eb
hpnightowl <iamhp2k@gmail.com>
2022-03-25 03:15:26 +0530

    note: remove command for removing entered title

--- server.js
+++ server.js
@@ -35,4 +35,31 @@ yargs.command({
     }
 })
 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        // saving in json file
+        const fs = require('fs');
+        // create notes.json if not exists
+        if (!fs.existsSync('notes.json')) {
+            fs.writeFileSync('notes.json', '[]');
+        }
+        // read notes.json
+        const notes = JSON.parse(fs.readFileSync('notes.json'));
+        // remove note
+        const newNotes = notes.filter(note => note.title !== argv.title);
+        // write notes.json
+        fs.writeFileSync('notes.json', JSON.stringify(newNotes));
+        console.log('Note removed successfully');
+    }
+})
+
 yargs.parse();
