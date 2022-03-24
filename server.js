4bfd1d5385e56df208049138c6bcadc488e5a591
hpnightowl <iamhp2k@gmail.com>
2022-03-25 02:44:16 +0530

    note: create notes.json for saving notes

--- server.js
+++ server.js
@@ -16,7 +16,22 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Adding note', argv.title, argv.body);
+        // saving in json file
+        const fs = require('fs');
+        // create notes.json if not exists
+        if (!fs.existsSync('notes.json')) {
+            fs.writeFileSync('notes.json', '[]');
+        }
+        // read notes.json
+        const notes = JSON.parse(fs.readFileSync('notes.json'));
+        // add new note
+        notes.push({
+            title: argv.title,
+            body: argv.body
+        });
+        // write notes.json
+        fs.writeFileSync('notes.json', JSON.stringify(notes));
+        console.log('Note added successfully');
     }
 })
 
